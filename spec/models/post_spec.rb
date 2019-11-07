c28ea20a42e5c3d56192acbfccd4f0edfa485f03
Vladimir Ionita <ionita.vladimir@gmail.com>
2019-11-07 16:32:40 +0100

    Add first test to `Post` model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not allow empty titles" do
+    post = Post.new
+    post.title = nil
+    post.save
+
+    expect(post.errors.count).to(eq(1))
+  end
 end
